46654a10ed56c973a7ceaf4061761e19d5cadfc9
Jose Fandiño <websysfan@gmail.com>
2020-11-20 21:56:44 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+
